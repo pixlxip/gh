4cbf1f872f29b4a161fc4c07774d9318e026d27e
Brayden Rischke <braydenrischke30@gmail.com>
2025-09-20 18:17:42 -0600

    Respond properly without a request body

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 export default {
   async fetch(request) {
-    const requestBodyReader = request.body.getReader();
-    const requestBodyArray = await requestBodyReader.read();
-    const decoder = new TextDecoder();
-    const requestBody = decoder.decode(requestBodyArray.value);
+    const requestBody = (() => {
+      if (request.body) {
+        const requestBodyReader = request.body.getReader();
+        const requestBodyArray = await requestBodyReader.read();
+        const decoder = new TextDecoder();
+        return decoder.decode(requestBodyArray.value);
+      } else return null;
+    })();
 
     const requestHeaders = request.headers.entries()
       .reduce((acc, [key, value]) => {
